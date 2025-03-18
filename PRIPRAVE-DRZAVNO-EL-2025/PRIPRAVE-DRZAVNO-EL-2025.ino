c3a511267f3e8ac2d29d4eacfd9142243cc3bb52
Luka Zamuda <luka.zamuda@sers.si>
2025-03-18 08:20:07 +0100

    servo test working

--- PRIPRAVE-DRZAVNO-EL-2025/PRIPRAVE-DRZAVNO-EL-2025.ino
+++ PRIPRAVE-DRZAVNO-EL-2025/PRIPRAVE-DRZAVNO-EL-2025.ino
@@ -1,9 +1,15 @@
+#include <Servo.h>  
+Servo myservo;
+
 void setup() {
   // put your setup code here, to run once:
-
+  myservo.attach(3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  myservo.write(0);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
 }
