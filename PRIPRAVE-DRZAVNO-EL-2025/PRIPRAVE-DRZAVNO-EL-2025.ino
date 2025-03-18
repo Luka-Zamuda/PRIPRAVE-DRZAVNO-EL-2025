2ee50110cfdd57aec0fcd734f6ae8d97285a31c1
Luka Zamuda <luka.zamuda@sers.si>
2025-03-18 07:27:41 +0100

    test led

--- PRIPRAVE-DRZAVNO-EL-2025/PRIPRAVE-DRZAVNO-EL-2025.ino
+++ PRIPRAVE-DRZAVNO-EL-2025/PRIPRAVE-DRZAVNO-EL-2025.ino
@@ -1,9 +1,18 @@
+#define LED_GREEN_PIN 4
+#define LED_YELLOW_PIN 5
+#define LED_RED_PIN 6
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_YELLOW_PIN, OUTPUT);
+  pinMode(LED_RED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_GREEN_PIN, HIGH);
+  digitalWrite(LED_YELLOW_PIN, HIGH);
+  digitalWrite(LED_RED_PIN, HIGH);
 
 }
