8dfba8af43a514988b84276a99f518e67d79f449
Luka Zamuda <luka.zamuda@sers.si>
2025-03-18 12:10:40 +0100

    non blocking

--- PRIPRAVE-DRZAVNO-EL-2025/PRIPRAVE-DRZAVNO-EL-2025.ino
+++ PRIPRAVE-DRZAVNO-EL-2025/PRIPRAVE-DRZAVNO-EL-2025.ino
@@ -1,9 +1,76 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * Simple demo, should work with any driver board
+ *
+ * Connect STEP, DIR as indicated
+ *
+ * Copyright (C)2015-2017 Laurentiu Badea
+ *
+ * This file may be redistributed under the terms of the MIT license.
+ * A copy of this license has been included with this distribution in the file LICENSE.
+ */
+#include <Arduino.h>
+#include "BasicStepperDriver.h"
 
+// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+#define MOTOR_STEPS 200
+#define RPM 120
+#define STOPPER_PIN 2
+// Since microstepping is set externally, make sure this matches the selected mode
+// If it doesn't, the motor will move at a different RPM than chosen
+// 1=full step, 2=half step etc.
+#define MICROSTEPS 1
+
+// All the wires needed for full functionality
+#define DIR 11
+#define STEP 12
+//Uncomment line to use enable/disable functionality
+//#define SLEEP 13
+
+// 2-wire basic config, microstepping is hardwired on the driver
+BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP);
+
+//Uncomment line to use enable/disable functionality
+//BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP, SLEEP);
+
+void setup() {
+    Serial.begin(115200);
+
+    // Configure stopper pin to read HIGH unless grounded
+    pinMode(STOPPER_PIN, INPUT_PULLUP);
+
+    stepper.begin(RPM, MICROSTEPS);
+    // if using enable/disable on ENABLE pin (active LOW) instead of SLEEP uncomment next line
+    // stepper.setEnableActiveState(LOW);
+    stepper.enable();
+
+    stepper.setRPM(40);
+
+    // let's say 100 complete revolutions (arbitrary number)
+    stepper.startMove(100 * MOTOR_STEPS * MICROSTEPS);     // in microsteps
+    // stepper.startRotate(100 * 360);                     // or in degrees
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // first, check if stopper was hit
+    if (digitalRead(STOPPER_PIN) == LOW){
+        Serial.println("STOPPER REACHED");
+
+        stepper.stop();
+        // stepper.startBrake();
+    }
+
+    // motor control loop - send pulse and return how long to wait until next pulse
+    unsigned wait_time_micros = stepper.nextAction();
+
+    // 0 wait time indicates the motor has stopped
+    if (wait_time_micros <= 0) {
+        stepper.disable();       // comment out to keep motor powered
+        delay(3600000);
+    }
+
+    // (optional) execute other code if we have enough time
+    if (wait_time_micros > 100){
+        // other code here
+    }
 
 }
